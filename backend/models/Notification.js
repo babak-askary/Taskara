d8109801c751f70fe657a64afe56536b551c1bb8
Matthew A Williams <157522802+Will7241@users.noreply.github.com>
2026-04-13 15:46:18 -0400

    Create Notification model file for backend

---
+++ backend/models/Notification.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+class Notification extends Model {}
+
+Notification.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  message: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  relatedTaskId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  isRead: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+}, {
+  sequelize,
+  modelName: 'Notification',
+  timestamps: true,
+});
+
+module.exports = Notification;
